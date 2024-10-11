a2c5fef8a2194700bca194ef92a7dd8d45fd8e02
Isabelle Santin <isabelle@chosenfewsoftware.com>
2024-10-11 10:10:58 -0500

    reeetry

--- QuicheStream.cs
+++ QuicheStream.cs
@@ -18,7 +18,7 @@ namespace Quiche.NET
             Unidirectional = 0x2
         }
 
-        private const int MAX_READ_RETRIES = 50;
+        private const int MAX_READ_RETRIES = 1000;
 
         private readonly Pipe? recvPipe, sendPipe;
 
